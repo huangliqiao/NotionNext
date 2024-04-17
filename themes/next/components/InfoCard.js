aa542ca064acad5eedc7d851b322dcd5c2bb2793
黄栎桥 <huangliqiao@HLQ-MacBook-Home.local>
2024-04-17 22:44:13 +0800

    update

--- themes/next/components/InfoCard.js
+++ themes/next/components/InfoCard.js
@@ -7,9 +7,11 @@ const InfoCard = (props) => {
   const { siteInfo } = props
   return <>
     <div className='flex flex-col items-center justify-center '>
-        // <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer' onClick={ () => { Router.push('/') }}>
+        {/* <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer' onClick={ () => { Router.push('/') }}>
+          <LazyImage src={siteInfo?.icon} className='rounded-full' width={120} alt={siteConfig('AUTHOR')}/>
+        </div> */}
         <div className='hover:cursor-pointer' onClick={ () => { Router.push('/about') }}>
-        <LazyImage src={siteInfo?.icon} /* className='rounded-full' */ width={120} alt={siteConfig('AUTHOR')}/>
+          <LazyImage src={siteInfo?.icon} width={120} alt={siteConfig('AUTHOR')}/>
         </div>
         <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
         <div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center'>{siteConfig('BIO')}</div>
