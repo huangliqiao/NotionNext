2d2ea4ed12faa67f81379daaa1ad2edacf00d182
huangliqiao <48161430@qq.com>
2024-04-17 22:35:29 +0800

    Update InfoCard.js

--- themes/next/components/InfoCard.js
+++ themes/next/components/InfoCard.js
@@ -7,8 +7,9 @@ const InfoCard = (props) => {
   const { siteInfo } = props
   return <>
     <div className='flex flex-col items-center justify-center '>
-        <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer' onClick={ () => { Router.push('/') }}>
-        <LazyImage src={siteInfo?.icon} className='rounded-full' width={120} alt={siteConfig('AUTHOR')}/>
+        // <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer' onClick={ () => { Router.push('/') }}>
+        <div className='hover:cursor-pointer' onClick={ () => { Router.push('/about') }}>
+        <LazyImage src={siteInfo?.icon} /* className='rounded-full' */ width={120} alt={siteConfig('AUTHOR')}/>
         </div>
         <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
         <div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center'>{siteConfig('BIO')}</div>
